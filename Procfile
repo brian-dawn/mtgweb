fd7d37523879f375f763f6ed7c01346b5d0daa76
Brian Dawn <brian.t.dawn@gmail.com>
2015-08-27 18:49:39 -0500

    Initial commit.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/mtgweb.jar clojure.main -m mtgweb.core
---
+++ env/prod/cljs/mtgweb/prod.cljs
@@ -0,0 +1,7 @@
+(ns mtgweb.app
+  (:require [mtgweb.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
